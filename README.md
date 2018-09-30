2e9f0dbb453c6d55c975b898e354fee0c55b0f5f
zuozishi <wwb123123@outlook.com>
2018-09-30 08:09:51 +0800

    0.0.8 update

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 The C51 Program Compile 
 
+## Language snippets
+
+* `#uchar`: 'typedef unsigned char uchar;'
+* `#uint`: 'typedef unsigned char uint;'
+* `#inc`: '#include <reg52.h>'
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `C51.binDir`: The C51 Bin Directory
+* `C51.OnlyOutputHexFile`: Only Output Hex File
+
 ## How to use
 
 0. Before you start, Setting the C51 bin dir.
@@ -16,12 +29,6 @@ The C51 Program Compile
 3. If your program no error, the hex file will created in the current dir.
 ![Complete!!!](https://raw.githubusercontent.com/Zuozishi/C51-Extension-for-VScode/master/image/3.jpg)
 
-## Extension Settings
-
-This extension contributes the following settings:
-
-* `C51.binDir`: The C51 Bin Directory
-
 ## Known Issues
 
 If some error on installing, please update vscode.
@@ -30,24 +37,30 @@ Program file name must have once '.c'.
 
 E.x : File name not support 'test.c.c'.
 
+### 0.0.8
+
+1. 语法高亮支持.
+2. 自动补全支持.
+3. 修复编译时终端窗口不清空问题.
+
 ### 0.0.5
 
-Fix .C file not support error.
+修复大写 '.C' 文件不支持的问题.
 (Thanks @wangnengquan's issue on github)
 
 ### 0.0.4
 
-Add github link.
+添加Github链接.
 
 ### 0.0.3
 
-Update README.md file (Add block "How to use?").
+在README.me中添加“How to use ?”信息.
 
 ### 0.0.2
 
-Add "Build C51" context menu for .c file.
-Add "Only output hex file" Setting.
+在 .c 文件编辑窗口中添加“Build C51”右键选项.
+添加 "Only output hex file" 设置.
 
 ### 0.0.1
 
-First Release
+第一个发行版本.
--- extension.js
+++ extension.js
@@ -1,45 +1,46 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+/*
+const C51 = [
+    { scheme: 'file', language: 'c' },
+    { scheme: 'untitled', language: 'c' }
+];
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+export class C51HoverProvider {
+    provideHover(document, position, token) {
+        let range=document.getWordRangeAtPosition(position);
+        let text=document.getText(range);
+        return new vscode.Hover("");
+    }
+}
+*/
+
 function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "c51" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
+    console.log('Congratulations, extension "c51" is now active!');
+    //let hover = vscode.languages.registerHoverProvider(C51, new C51HoverProvider.C51HoverProvider());
+    //context.subscriptions.push(hover);
+    
     let disposable = vscode.commands.registerCommand('extension.buildC51', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
         var cmd=null;
+        //let digs=vscode.languages.getDiagnostics();
         for (let i = 0; i < vscode.window.terminals.length; i++) {
-            if(vscode.window.terminals[i].name=="CMD")cmd=vscode.window.terminals[i];
+            if(vscode.window.terminals[i].name=="C51 CMD")
+            {
+                cmd=vscode.window.terminals[i];
+                cmd.dispose();
+            }
         }
-        if(cmd==null){
-            cmd=vscode.window.createTerminal("CMD","C:\\Windows\\System32\\cmd.exe");
-        }
+        cmd=vscode.window.createTerminal("C51 CMD","C:\\Windows\\System32\\cmd.exe");
         cmd.show();
-        var log=vscode.window.createOutputChannel("C51");
         var binDir=vscode.workspace.getConfiguration("C51").get("binDir");
         var hexFile=vscode.workspace.getConfiguration("C51").get("OnlyOutputHexFile");
-        var workDir=vscode.workspace.rootPath;
-        if(workDir==undefined){
-            vscode.window.showErrorMessage("请先打开工作目录");
-            return;
-        }
-        if(vscode.workspace.textDocuments.length<=0){
-            vscode.window.showErrorMessage("请先打开一个文件");
+        if(vscode.window.activeTextEditor==null || vscode.window.activeTextEditor.document==null){
+            vscode.window.showErrorMessage("Please open a c file.");
             return;
         }
         var filePath=vscode.workspace.textDocuments[0].fileName;
         filePath=filePath.replace(".c","");
-        cmd.sendText("cls");
+        cmd.sendText("@echo off");
         cmd.sendText("\""+binDir+"\\C51.exe"+"\" "+"\""+filePath+".c\"",true);
         cmd.sendText("\""+binDir+"\\BL51.exe"+"\" "+"\""+filePath+".OBJ\"",true);
         cmd.sendText("\""+binDir+"\\OH51.exe"+"\" "+"\""+filePath+"\"",true);
@@ -50,11 +51,11 @@ function activate(context) {
             cmd.sendText("del "+filePath);
         }
     });
+    
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {
 }
 exports.deactivate = deactivate;
